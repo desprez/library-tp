0156a86def12a3f00d3aa8f5aa873e51bcf0a66d
pascal.desprez <pascal.desprez@fr.ibm.com>
2021-01-14 13:45:43 +0100

    add ISBN attribute

--- src/main/java/fr/training/spring/Library/domain/exception/ErrorCodes.java
+++ src/main/java/fr/training/spring/Library/domain/exception/ErrorCodes.java
@@ -3,4 +3,6 @@ public final class ErrorCodes {
 	private ErrorCodes() {}
 
 	public static final String LIBRARY_NOT_FOUND = "ERR_0001";
+
+	public static final String BOOK_NOT_FOUND = "ERR_0002";
 }
