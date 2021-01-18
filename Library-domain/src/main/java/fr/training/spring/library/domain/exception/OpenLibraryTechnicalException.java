cf8d4961c34b6b72971ec7a65e71204e03944f6c
pascal.desprez <pascal.desprez@fr.ibm.com>
2021-01-18 14:53:16 +0100

    tp-spring-11

--- Library-domain/src/main/java/fr/training/spring/library/domain/exception/OpenLibraryTechnicalException.java
+++ Library-domain/src/main/java/fr/training/spring/library/domain/exception/OpenLibraryTechnicalException.java
@@ -1,12 +1,10 @@
 package fr.training.spring.library.domain.exception;
 
-import org.springframework.web.client.HttpStatusCodeException;
-
 public class OpenLibraryTechnicalException extends TechnicalException {
 
 	private static final long serialVersionUID = 1L;
 
-	public OpenLibraryTechnicalException(final HttpStatusCodeException e) {
+	public OpenLibraryTechnicalException(final Exception e) {
 		super(e);
 	}
 }
--- Library-domain/src/main/java/fr/training/spring/library/domain/exception/TechnicalException.java
+++ Library-domain/src/main/java/fr/training/spring/library/domain/exception/TechnicalException.java
@@ -1,12 +1,10 @@
 package fr.training.spring.library.domain.exception;
 
-import org.springframework.web.client.HttpStatusCodeException;
-
 public class TechnicalException extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
 
-	public TechnicalException(final HttpStatusCodeException e) {
+	public TechnicalException(final Exception e) {
 		super(e);
 	}
 }
